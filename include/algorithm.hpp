2f6664a8fa6d37c73bc3d4253d3adae7c505ab38
engelphi <philipp.engel.1990@googlemail.com>
2017-12-11 19:07:10 +0100

    update format style

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -7,14 +7,14 @@ template <typename InputIterator, typename OutputIterator, typename UnaryOp,
 OutputIterator transform_if(InputIterator first, InputIterator last,
                             OutputIterator d_first, UnaryOp unary_op,
                             Pred predicate) {
-  while (first != last) {
-    if (predicate(*first)) {
-      *d_first++ = unary_op(*first++);
-    } else {
-      *d_first++ = *first++;
+    while (first != last) {
+        if (predicate(*first)) {
+            *d_first++ = unary_op(*first++);
+        } else {
+            *d_first++ = *first++;
+        }
     }
-  }
-  return d_first;
+    return d_first;
 }
 
 } // namespace algo
