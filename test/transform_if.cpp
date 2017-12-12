838cb44f591c1925a45be0aa183f13f959c9f009
engelphi <philipp.engel.1990@googlemail.com>
2017-12-12 12:30:28 +0100

    extend transform unit tests

--- test/transform_if.cpp
+++ test/transform_if.cpp
@@ -1,5 +1,9 @@
 #include <algorithm.hpp>
+#include <deque>
+#include <forward_list>
 #include <gtest/gtest.h>
+#include <list>
+#include <vector>
 
 TEST(transform_if,
      should_transform_elements_of_vector_inplace_if_predicate_matches) {
@@ -22,3 +26,59 @@ TEST(transform_if, should_transform_elements_of_vector_if_predicate_matches) {
 
   ASSERT_EQ(expected_result, result);
 }
+
+TEST(transform_if,
+     should_transform_elements_of_deque_inplace_if_predicate_matches) {
+    std::deque<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::deque<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+    algo::transform_if(elems.begin(), elems.end(), elems.begin(),
+                       [](int &elem) { return elem + 1; },
+                       [](const int &elem) { return (elem % 2) != 0; });
+
+    ASSERT_EQ(expected_result, elems);
+}
+
+TEST(transform_if, should_transform_elements_of_deque_if_predicate_matches) {
+    std::deque<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::deque<int> result{};
+    std::deque<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+    algo::transform_if(elems.begin(), elems.end(), std::back_inserter(result),
+                       [](int &elem) { return elem + 1; },
+                       [](const int &elem) { return (elem % 2) != 0; });
+
+    ASSERT_EQ(expected_result, result);
+}
+
+TEST(transform_if,
+     should_transform_elements_of_list_inplace_if_predicate_matches) {
+    std::list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+    algo::transform_if(elems.begin(), elems.end(), elems.begin(),
+                       [](int &elem) { return elem + 1; },
+                       [](const int &elem) { return (elem % 2) != 0; });
+
+    ASSERT_EQ(expected_result, elems);
+}
+
+TEST(transform_if, should_transform_elements_of_list_if_predicate_matches) {
+    std::list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::list<int> result{};
+    std::list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+    algo::transform_if(elems.begin(), elems.end(), std::back_inserter(result),
+                       [](int &elem) { return elem + 1; },
+                       [](const int &elem) { return (elem % 2) != 0; });
+
+    ASSERT_EQ(expected_result, result);
+}
+
+TEST(transform_if,
+     should_transform_elements_of_forward_list_inplace_if_predicate_matches) {
+    std::forward_list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::forward_list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+    algo::transform_if(elems.begin(), elems.end(), elems.begin(),
+                       [](int &elem) { return elem + 1; },
+                       [](const int &elem) { return (elem % 2) != 0; });
+
+    ASSERT_EQ(expected_result, elems);
+}
+
