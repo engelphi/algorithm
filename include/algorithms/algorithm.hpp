3fd4352680b2b6fdf1801ee10a7dbc815bbc6dff
engelphi <philipp.engel.1990@googlemail.com>
2017-12-15 14:30:35 +0100

    add unzip algorithm and tests

--- include/algorithms/algorithm.hpp
+++ include/algorithms/algorithm.hpp
@@ -3,4 +3,5 @@
  */
 #pragma once
 #include "internal/transform_if.hpp"
+#include "internal/unzip.hpp"
 #include "internal/zip.hpp"
---
+++ include/algorithms/internal/unzip.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file unzip.hpp
+ */
+#pragma once
+#include <utility>
+
+namespace algo {
+
+/**
+ * Unzips a range of pairs into two output ranges.
+ * @tparam InputIterator The iterator type of the input range
+ * @tparam OutputIterator1 The iterator type of the first output range.
+ * @tparam OutputIterator2 The iterator type of the second output range.
+ * @param first The start of the input range.
+ * @param last The last element of the input range.
+ * @param out1_first The start of the first output range.
+ * @param out2_first The start of the second output range.
+ * @return Returns a pair containing the past the last element iterators of the
+ *         two output ranges
+ */
+template <typename InputIterator, typename OutputIterator1,
+          typename OutputIterator2>
+decltype(auto) unzip(InputIterator first, InputIterator last,
+                     OutputIterator1 out1_first, OutputIterator2 out2_first) {
+    while (first != last) {
+        (*out1_first++) = (*first).first;
+        (*out2_first++) = (*first).second;
+        first++;
+    }
+    return std::pair(out1_first, out2_first);
+}
+
+} // namespace algo
