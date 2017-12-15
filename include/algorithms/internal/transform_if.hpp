32686fd26164f17415a371a0afddc4b1fbf75f58
engelphi <philipp.engel.1990@googlemail.com>
2017-12-15 13:23:49 +0100

    add documentation again

--- include/algorithms/internal/transform_if.hpp
+++ include/algorithms/internal/transform_if.hpp
@@ -1,6 +1,24 @@
+/**
+ * @file transform_if.hpp
+ */
 #pragma once
 
 namespace algo {
+
+/**
+ * Transforms the elements of the input range if the predicate is evaluates to
+ * true. The resulting elements are stored to the output range.
+ * @tparam InputIterator The type of the input iterator.
+ * @tparam OutputIterator The type of the output iterator.
+ * @tparam UnaryOp The type of transformation function.
+ * @tparam Pred The type of the predicate function.
+ * @param first The start of input range.
+ * @param last The end of the input range.
+ * @param d_first The start of the output range.
+ * @param unary_op The transformation function.
+ * @param predicate The predicate function.
+ * @return Returns an iterator to past the last element of the output range
+ */
 template <typename InputIterator, typename OutputIterator, typename UnaryOp,
           typename Pred>
 OutputIterator transform_if(InputIterator first, InputIterator last,
