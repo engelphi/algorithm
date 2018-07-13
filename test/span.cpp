225d3d82211c1b3d16e696b0b41d91d0b2ea090d
Philipp Engel <philipp.engel.1990@googlemail.com>
2018-07-13 09:47:06 +0200

    refactor intersperse, span and transform_if tests

--- test/span.cpp
+++ test/span.cpp
@@ -8,190 +8,199 @@
 #include <list>
 #include <vector>
 
-TEST_CASE("span of empty range leaves output ranges empty (vector)", "[span]") {
-    std::vector<int> input{};
-    std::vector<int> outputTrue{};
-    std::vector<int> outputFalse{};
-    std::vector<int> expectedTrue{};
-    std::vector<int> expectedFalse{};
+TEST_CASE("span", "[span]") {
+    SECTION("span of empty range leaves output ranges empty") {
+        SECTION("(vector)") {
+            std::vector<int> input{};
+            std::vector<int> outputTrue{};
+            std::vector<int> outputFalse{};
+            std::vector<int> expectedTrue{};
+            std::vector<int> expectedFalse{};
 
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
 
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(list)") {
+            std::list<int> input{};
+            std::list<int> outputTrue{};
+            std::list<int> outputFalse{};
+            std::list<int> expectedTrue{};
+            std::list<int> expectedFalse{};
+
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(deque)") {
+            std::deque<int> input{};
+            std::deque<int> outputTrue{};
+            std::deque<int> outputFalse{};
+            std::deque<int> expectedTrue{};
+            std::deque<int> expectedFalse{};
+
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(forward_list)") {
+            std::forward_list<int> input{};
+            std::forward_list<int> outputTrue{};
+            std::forward_list<int> outputFalse{};
+            std::forward_list<int> expectedTrue{};
+            std::forward_list<int> expectedFalse{};
+
+            algo::span(input.begin(), input.end(), outputTrue.begin(),
+                       outputFalse.begin(),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(array)") {
+            std::array<int, 1> input{};
+            std::array<int, 1> outputTrue{};
+            std::array<int, 1> outputFalse{};
+            std::array<int, 1> expectedTrue{};
+            std::array<int, 1> expectedFalse{};
+
+            algo::span(input.begin(), input.end(), outputTrue.begin(),
+                       outputFalse.begin(),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(mixed)") {
+            std::vector<int> input{};
+            std::vector<int> outputTrue{};
+            std::list<int> outputFalse{};
+            std::vector<int> expectedTrue{};
+            std::list<int> expectedFalse{};
+
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+    }
+
+    SECTION("span split elements to ranges according to predicate") {
+        SECTION("(vector)") {
+            std::vector<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::vector<int> outputTrue{};
+            std::vector<int> outputFalse{};
+            std::vector<int> expectedTrue{1, 2, 3, 4};
+            std::vector<int> expectedFalse{5, 6, 7, 8, 9, 10};
+
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(list)") {
+            std::list<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::list<int> outputTrue{};
+            std::list<int> outputFalse{};
+            std::list<int> expectedTrue{1, 2, 3, 4};
+            std::list<int> expectedFalse{5, 6, 7, 8, 9, 10};
+
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(deque)") {
+            std::deque<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::deque<int> outputTrue{};
+            std::deque<int> outputFalse{};
+            std::deque<int> expectedTrue{1, 2, 3, 4};
+            std::deque<int> expectedFalse{5, 6, 7, 8, 9, 10};
+
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(forward_list)") {
+            std::forward_list<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::forward_list<int> outputTrue{};
+            outputTrue.resize(4);
+            std::forward_list<int> outputFalse{};
+            outputFalse.resize(6);
+            std::forward_list<int> expectedTrue{1, 2, 3, 4};
+            std::forward_list<int> expectedFalse{5, 6, 7, 8, 9, 10};
+
+            algo::span(input.begin(), input.end(), outputTrue.begin(),
+                       outputFalse.begin(),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(array)") {
+            std::array<int, 10> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::array<int, 4> outputTrue{};
+            std::array<int, 6> outputFalse{};
+            std::array<int, 4> expectedTrue{1, 2, 3, 4};
+            std::array<int, 6> expectedFalse{5, 6, 7, 8, 9, 10};
+
+            algo::span(input.begin(), input.end(), outputTrue.begin(),
+                       outputFalse.begin(),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+
+        SECTION("(mixed)") {
+            std::vector<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::vector<int> outputTrue{};
+            std::list<int> outputFalse{};
+            std::vector<int> expectedTrue{1, 2, 3, 4};
+            std::list<int> expectedFalse{5, 6, 7, 8, 9, 10};
+
+            algo::span(input.begin(), input.end(),
+                       std::back_inserter(outputTrue),
+                       std::back_inserter(outputFalse),
+                       [](const auto &x) { return x < 5; });
+
+            REQUIRE(expectedTrue == outputTrue);
+            REQUIRE(expectedFalse == outputFalse);
+        }
+    }
 }
 
-TEST_CASE("span split elements to ranges according to predicate (vector)",
-          "[span]") {
-    std::vector<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::vector<int> outputTrue{};
-    std::vector<int> outputFalse{};
-    std::vector<int> expectedTrue{1, 2, 3, 4};
-    std::vector<int> expectedFalse{5, 6, 7, 8, 9, 10};
-
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span of empty range leaves output ranges empty (list)", "[span]") {
-    std::list<int> input{};
-    std::list<int> outputTrue{};
-    std::list<int> outputFalse{};
-    std::list<int> expectedTrue{};
-    std::list<int> expectedFalse{};
-
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span split elements to ranges according to predicate (list)",
-          "[span]") {
-    std::list<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::list<int> outputTrue{};
-    std::list<int> outputFalse{};
-    std::list<int> expectedTrue{1, 2, 3, 4};
-    std::list<int> expectedFalse{5, 6, 7, 8, 9, 10};
-
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span of empty range leaves output ranges empty (deque)", "[span]") {
-    std::deque<int> input{};
-    std::deque<int> outputTrue{};
-    std::deque<int> outputFalse{};
-    std::deque<int> expectedTrue{};
-    std::deque<int> expectedFalse{};
-
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span split elements to ranges according to predicate (deque)",
-          "[span]") {
-    std::deque<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::deque<int> outputTrue{};
-    std::deque<int> outputFalse{};
-    std::deque<int> expectedTrue{1, 2, 3, 4};
-    std::deque<int> expectedFalse{5, 6, 7, 8, 9, 10};
-
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span of empty range leaves output ranges empty (forward_list)",
-          "[span]") {
-    std::forward_list<int> input{};
-    std::forward_list<int> outputTrue{};
-    std::forward_list<int> outputFalse{};
-    std::forward_list<int> expectedTrue{};
-    std::forward_list<int> expectedFalse{};
-
-    algo::span(input.begin(), input.end(), outputTrue.begin(),
-               outputFalse.begin(), [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span split elements to ranges according to predicate (forward_list)",
-          "[span]") {
-    std::forward_list<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::forward_list<int> outputTrue{};
-    outputTrue.resize(4);
-    std::forward_list<int> outputFalse{};
-    outputFalse.resize(6);
-    std::forward_list<int> expectedTrue{1, 2, 3, 4};
-    std::forward_list<int> expectedFalse{5, 6, 7, 8, 9, 10};
-
-    algo::span(input.begin(), input.end(), outputTrue.begin(),
-               outputFalse.begin(), [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span of empty range leaves output ranges empty (array)", "[span]") {
-    std::array<int, 1> input{};
-    std::array<int, 1> outputTrue{};
-    std::array<int, 1> outputFalse{};
-    std::array<int, 1> expectedTrue{};
-    std::array<int, 1> expectedFalse{};
-
-    algo::span(input.begin(), input.end(), outputTrue.begin(),
-               outputFalse.begin(), [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span split elements to ranges according to predicate (array)",
-          "[span]") {
-    std::array<int, 10> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::array<int, 4> outputTrue{};
-    std::array<int, 6> outputFalse{};
-    std::array<int, 4> expectedTrue{1, 2, 3, 4};
-    std::array<int, 6> expectedFalse{5, 6, 7, 8, 9, 10};
-
-    algo::span(input.begin(), input.end(), outputTrue.begin(),
-               outputFalse.begin(), [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span of empty range leaves output ranges of different types empty "
-          "(vector)",
-          "[span]") {
-    std::vector<int> input{};
-    std::vector<int> outputTrue{};
-    std::list<int> outputFalse{};
-    std::vector<int> expectedTrue{};
-    std::list<int> expectedFalse{};
-
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
-
-TEST_CASE("span split elements to ranges of different types according to "
-          "predicate (vector)",
-          "[span]") {
-    std::vector<int> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::vector<int> outputTrue{};
-    std::list<int> outputFalse{};
-    std::vector<int> expectedTrue{1, 2, 3, 4};
-    std::list<int> expectedFalse{5, 6, 7, 8, 9, 10};
-
-    algo::span(input.begin(), input.end(), std::back_inserter(outputTrue),
-               std::back_inserter(outputFalse),
-               [](const auto &x) { return x < 5; });
-
-    REQUIRE(expectedTrue == outputTrue);
-    REQUIRE(expectedFalse == outputFalse);
-}
--- test/transform_if.cpp
+++ test/transform_if.cpp
@@ -7,89 +7,86 @@
 #include <list>
 #include <vector>
 
-TEST_CASE("transform_if should transform elements of vector inplace if "
-          "predicate matches",
-          "[transform_if]") {
-    std::vector<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::vector<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
-    algo::transform_if(elems.begin(), elems.end(), elems.begin(),
-                       [](int &elem) { return elem + 1; },
-                       [](const int &elem) { return (elem % 2) != 0; });
+TEST_CASE("transform_if", "[transform_if]") {
+    SECTION(
+        "should transform elements of container inplace if predicate matches") {
+        SECTION("(vector)") {
+            std::vector<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::vector<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+            algo::transform_if(elems.begin(), elems.end(), elems.begin(),
+                               [](int &elem) { return elem + 1; },
+                               [](const int &elem) { return (elem % 2) != 0; });
 
-    REQUIRE(expected_result == elems);
+            REQUIRE(expected_result == elems);
+        }
+
+        SECTION("(deque)") {
+            std::deque<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::deque<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+            algo::transform_if(elems.begin(), elems.end(), elems.begin(),
+                               [](int &elem) { return elem + 1; },
+                               [](const int &elem) { return (elem % 2) != 0; });
+
+            REQUIRE(expected_result == elems);
+        }
+
+        SECTION("(list)") {
+            std::list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+            algo::transform_if(elems.begin(), elems.end(), elems.begin(),
+                               [](int &elem) { return elem + 1; },
+                               [](const int &elem) { return (elem % 2) != 0; });
+
+            REQUIRE(expected_result == elems);
+        }
+
+        SECTION("(forward_list)") {
+            std::forward_list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::forward_list<int> expected_result{2, 2, 4, 4,  6,
+                                                   6, 8, 8, 10, 10};
+            algo::transform_if(elems.begin(), elems.end(), elems.begin(),
+                               [](int &elem) { return elem + 1; },
+                               [](const int &elem) { return (elem % 2) != 0; });
+
+            REQUIRE(expected_result == elems);
+        }
+    }
+
+    SECTION("should transform elements of vector if predicate matches") {
+        SECTION("(vector)") {
+            std::vector<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::vector<int> result{};
+            std::vector<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+            algo::transform_if(elems.begin(), elems.end(),
+                               std::back_inserter(result),
+                               [](int &elem) { return elem + 1; },
+                               [](const int &elem) { return (elem % 2) != 0; });
+
+            REQUIRE(expected_result == result);
+        }
+
+        SECTION("(deque)") {
+            std::deque<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::deque<int> result{};
+            std::deque<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+            algo::transform_if(elems.begin(), elems.end(),
+                               std::back_inserter(result),
+                               [](int &elem) { return elem + 1; },
+                               [](const int &elem) { return (elem % 2) != 0; });
+
+            REQUIRE(expected_result == result);
+        }
+
+        SECTION("(list)") {
+            std::list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            std::list<int> result{};
+            std::list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
+            algo::transform_if(elems.begin(), elems.end(),
+                               std::back_inserter(result),
+                               [](int &elem) { return elem + 1; },
+                               [](const int &elem) { return (elem % 2) != 0; });
+
+            REQUIRE(expected_result == result);
+        }
+    }
 }
-
-TEST_CASE(
-    "transform_if should transform elements of vector if predicate matches",
-    "[transform_if]") {
-    std::vector<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::vector<int> result{};
-    std::vector<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
-    algo::transform_if(elems.begin(), elems.end(), std::back_inserter(result),
-                       [](int &elem) { return elem + 1; },
-                       [](const int &elem) { return (elem % 2) != 0; });
-
-    REQUIRE(expected_result == result);
-}
-
-TEST_CASE("transform_if should transform elements of deque inplace if "
-          "predicate matches",
-          "[transform_if]") {
-    std::deque<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::deque<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
-    algo::transform_if(elems.begin(), elems.end(), elems.begin(),
-                       [](int &elem) { return elem + 1; },
-                       [](const int &elem) { return (elem % 2) != 0; });
-
-    REQUIRE(expected_result == elems);
-}
-
-TEST_CASE(
-    "transform_if should transform elements of deque if predicate matches",
-    "[transform_if]") {
-    std::deque<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::deque<int> result{};
-    std::deque<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
-    algo::transform_if(elems.begin(), elems.end(), std::back_inserter(result),
-                       [](int &elem) { return elem + 1; },
-                       [](const int &elem) { return (elem % 2) != 0; });
-
-    REQUIRE(expected_result == result);
-}
-
-TEST_CASE("transform_if should transform elements of list inplace if predicate "
-          "matches",
-          "[transform_if]") {
-    std::list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
-    algo::transform_if(elems.begin(), elems.end(), elems.begin(),
-                       [](int &elem) { return elem + 1; },
-                       [](const int &elem) { return (elem % 2) != 0; });
-
-    REQUIRE(expected_result == elems);
-}
-
-TEST_CASE("transform_if should transform elements of list if predicate matches",
-          "[transform_if]") {
-    std::list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::list<int> result{};
-    std::list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
-    algo::transform_if(elems.begin(), elems.end(), std::back_inserter(result),
-                       [](int &elem) { return elem + 1; },
-                       [](const int &elem) { return (elem % 2) != 0; });
-
-    REQUIRE(expected_result == result);
-}
-
-TEST_CASE("transform_if should transform elements of forward_list inplace if "
-          "predicate matches",
-          "[transform_if]") {
-    std::forward_list<int> elems{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::forward_list<int> expected_result{2, 2, 4, 4, 6, 6, 8, 8, 10, 10};
-    algo::transform_if(elems.begin(), elems.end(), elems.begin(),
-                       [](int &elem) { return elem + 1; },
-                       [](const int &elem) { return (elem % 2) != 0; });
-
-    REQUIRE(expected_result == elems);
-}
-
